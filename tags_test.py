b8035524ca468d5d5ad43ee8aa6bafd823cc5e31
Kaique da Silva <kaique.developer@gmail.com>
2017-02-22 14:09:10 -0300

    Tested the class existenece and equality

--- tags_test.py
+++ tags_test.py
@@ -7,6 +7,9 @@ class LinkTest(unittest.TestCase):
     def setUp(self):
         self.link = generate_component.Link()
 
+    def test_link_instantiation(self):
+        self.assertEqual(self.link.__class__,generate_component.Link)
+
     def test_link_str(self):
         self.assertEqual(self.link.__str__(),'<link rel="stylesheet" href="./css/component-example-standalone.css">')
 
