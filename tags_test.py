149792158f3314de110bdbd2d6216171e1efd5b7
Kaique da Silva <kaique.developer@gmail.com>
2017-02-22 15:54:28 -0300

    Covering the basic tests

--- tags_test.py
+++ tags_test.py
@@ -8,8 +8,21 @@ class LinkTest(unittest.TestCase):
     def setUp(self):
         self.link = generate_component.Link()
 
+    def test_link_instantiation(self):
+        self.assertEqual(self.link.__class__,generate_component.Link)
+
     def test_link_str(self):
         self.assertEqual(self.link.__str__(),'<link rel="stylesheet" href="./css/component-example-standalone.css">')
+
+class HeaderTest(unittest.TestCase):
+    def setUp(self):
+        self.header = generate_component.Header()
+
+    def test_header_instantiation(self):
+        self.assertEqual(self.header.__class__,generate_component.Header)
+
+    def test_header_str(self):
+        self.assertEqual(self.header.__str__(),'<h1>Component Example Standalone</h1>')
 
 if __name__ == '__main__' and 'DEBUG' in os.environ and os.environ['DEBUG'] == 'true':
     unittest.main(verbosity=2)
